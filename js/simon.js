18d92ed9197c59a47e92c9c431ca0802be8ade33
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-10 15:48:55 -0700

    Finished test game.

--- js/simon.js
+++ js/simon.js
@@ -1,7 +1,10 @@
 var colorArr = ['green', 'red', 'yellow', 'blue'];
+var playerTurn = false;
 var simonArr = [];
 var playerArr = [];
 var randomColor = 0;
+var playerGuess = '';
+var idx = 0;
 
 var greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
 var redSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
@@ -12,10 +15,34 @@ var sound = [greenSound, redSound, yellowSound, blueSound];
 function nextColor() {
 	randomColor = colorArr[Math.floor(Math.random() * 4)];
 	simonArr.push(randomColor);
+	console.log(simonArr);
+	playerTurn = true;
 	return simonArr;
 }
 
-
 $(document).ready(function() {
+	$('.game-container').on('click', '.gameboard', function(e) {
+		e.stopPropagation();
+		if (playerTurn === true) {
+			playerGuess = $(this)[0].id
+			playerArr.push(playerGuess);
+			console.log(playerArr);
+			if (simonArr[idx] === playerArr[idx]) {
+				if (idx === simonArr.length - 1) {
+					playerTurn = false;
+					playerArr = [];
+					idx = 0;
+					nextColor();
+				} else {
+					idx++;
+				}
+			} else {
+				console.log('Loser!');
+			}
+		}
+	});
 
+	$('button').on('click', function() {
+		nextColor();
+	});
 });
