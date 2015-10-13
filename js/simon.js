7c4ab4b05469843afc7864fbfdc7a78498f99199
thomasvaeth <thomas.vaeth@gmail.com>
2015-10-13 13:47:09 -0700

    Added beginning sequence.

--- js/simon.js
+++ js/simon.js
@@ -7,6 +7,7 @@ var playerGuess = '';
 var idx = 0;
 var count = 0;
 var gamePower = true;
+var startSequence = ['green', 'red', 'blue', 'green', 'yellow', 'red', 'blue', 'green', 'yellow', 'red', 'green', 'red', 'green', 'yellow', 'blue', 'red', 'green', 'blue', 'red', 'yellow', 'green', 'red', 'blue', 'yellow'];
 
 // Making variables for all audio that is needed.
 var greenSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
@@ -16,6 +17,29 @@ var blueSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3
 var soundsArr = [greenSound, redSound, yellowSound, blueSound];
 // Wrong sound isn't correct, but good enough for demo.
 var wrongSound = new Audio('https://www.freesound.org/data/previews/142/142608_1840739-lq.mp3');
+
+function startItUp(sequence) {
+	// First setTimeout is delay between pressing start and starting intro sequence.
+	setTimeout(function() {
+		for (var i = 0; i < sequence.length; i++) {
+			(function(order) {
+				// Second setTimeout is for delay between computer moves.
+				setTimeout(function() {
+					$('.' + sequence[order]).addClass(sequence[order] + '-glow');
+					// Third setTimeout is for delay between removing glow effect.
+					setTimeout(function() {
+						$('.' + sequence[order]).removeClass(sequence[order] + '-glow')
+					}, 200);
+				}, order * 250);
+			})(i);
+		}
+	}, 100);
+	// Fourth setTimeout is for delay before starting game waiting for initial sequence to end.
+	setTimeout(function() {
+		nextColor();
+		lightAndSound(simonArr);
+	}, 7100);
+}
 
 // Getting a color and pushing it to an array of computer moves.
 function nextColor() {
@@ -40,7 +64,7 @@ function lightAndSound(simon) {
 					setTimeout(function() {
 						$('.' + simon[order]).removeClass(simon[order] + '-glow')
 					}, 500);
-				}, order * 750);
+				}, order * 550);
 			})(i);
 		}
 	}, 1000);
@@ -97,8 +121,7 @@ $(document).ready(function() {
 		if (gamePower === true) {
 			gamePower = false;
 			$('.count').html('--');
-			nextColor();
-			lightAndSound(simonArr);
+			startItUp(startSequence);
 		} else {
 			gamePower = true;
 			$('.count').html('');
